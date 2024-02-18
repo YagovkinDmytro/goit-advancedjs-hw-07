a30f1f4256cc83e72e43eaf6f23bed095c4e2c24
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2024-02-18 10:14:03 +0100

    done

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,54 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  public getKey(): number {
+    return this.key.getSignature();
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  private tenants: Person[] = [];
+
+  constructor(public key: Key) {}
+
+  public comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+    return console.log(this.tenants);
+  }
+  public abstract openDoor(key: number): void;
+}
+
+class MyHouse extends House {
+  door = false;
+  constructor(key: Key) {
+    super(key);
+  }
+
+  public openDoor(key: number): void {
+    if (key === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
-const house = new MyHouse(key);
+const hause = new MyHouse(key);
 const person = new Person(key);
 
-house.openDoor(person.getKey());
-
-house.comeIn(person);
-
+hause.openDoor(person.getKey());
+hause.comeIn(person);
 
 export {};
